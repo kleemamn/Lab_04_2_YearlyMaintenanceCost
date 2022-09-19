65eb4c372d28219eb15bc080214a2c540f15af44
kleemamn <kleemamn@mail.uc.edu>
2022-09-18 21:36:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double costSpring = 10 ;
+        double costSummer = 15;
+        double costFall = 20;
+        double costWinter = 15;
+        double costTotal ;
+
+        costTotal = costSpring + costSummer + costFall + costWinter;
+
+        System.out.println("When the cost of spring is " + costSpring + " and the cost of summer is " + costSummer + " and the cost of fall is " + costFall + " and the cost of winter is " + costWinter + ", the total yearly cost will be " + costTotal );
+
+    }
+}
